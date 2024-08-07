1c520a08e46f9106c1c24cbc48a9964b15e30a08
Laurence Ramirez <rhenz8823@gmail.com>
2024-08-08 01:19:19 +0800

    Login-Register-ForgotPassword

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login () {
+        return view('auth.login');
+    }
+
+    public function register () {
+        return view ('auth.register');
+    }
+
+    public function forgot () {
+        return view ('auth.forgot');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
-
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//* route for login page
+
+Route::get('login', [AuthController::class, 'login']);
+Route::get('register', [AuthController::class, 'register']);
+Route::get('forgot', [AuthController::class, 'forgot']);
